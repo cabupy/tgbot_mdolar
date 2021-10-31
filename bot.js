ed5377d00ec0d41e3b25cb28ac71723e4bb32939
Carlos Vallejos <cabupy@gmail.com>
2021-10-30 23:26:59 -0300

    Agregamos control de anonimos

--- bot.js
+++ bot.js
@@ -5,36 +5,46 @@ const moment = require('moment')
 
 const bot = new Telegraf(process.env.TOKEN)
 
-bot.start((ctx) =>{
+bot.start((ctx) => {
   console.log(`start: ${ctx.from.username}`)
-  ctx.replyWithMarkdown(`Bienvenido ${ctx.from.username} al 🤖 *MejorDolarPy*
+  if (!ctx.from.username) {
+    ctx.reply('🤖 : No converso con anónimos :(')
+  } else {
+    ctx.replyWithMarkdown(`Bienvenido ${ctx.from.username} al 🤖 *MejorDolarPy*
 
-  *Lista de comandos*:
-  /start para iniciar conversación.
-  /mejordolar para solicitar las cotizaciones.
-  /md para solicitar las cotizaciones (abreviado).
-  /help para solicitar ayuda.`)
+    *Lista de comandos*:
+
+    /start para iniciar conversación.
+    /mejordolar para solicitar las cotizaciones.
+    /md para solicitar las cotizaciones (abreviado).
+    /help para solicitar ayuda.`)
+  }
 })
 
 bot.help((ctx) => {
   console.log(`help: ${ctx.from.username}`)
-  ctx.replyWithMarkdown(`*Lista de comandos*:
-  /start para iniciar conversación.
-  /mejordolar para solicitar las cotizaciones.
-  /md para solicitar las cotizaciones (abreviado).
-  /help para solicitar ayuda.`)
+  if (!ctx.from.username) {
+    ctx.reply('🤖 : No converso con anónimos :(')
+  } else {
+    ctx.replyWithMarkdown(`*Lista de comandos*:
+
+    /start para iniciar conversación.
+    /mejordolar para solicitar las cotizaciones.
+    /md para solicitar las cotizaciones (abreviado).
+    /help para solicitar ayuda.`)
+  }
 })
 
 bot.command(['mejordolar', 'md'], async (ctx) => {
-  if (ctx.from.is_bot) {
-    ctx.reply('No converso con otros bots.')
+  if (ctx.from.is_bot || !ctx.from.username) {
+    ctx.reply('No converso con anónimos ni con otros bots.')
   } else {
     console.log(`md: ${ctx.from.username}`)
     const ahora = new Date()
     const cotizaciones = await axios(process.env.APIURL)
     let impresion
-    impresion = `*Hola ${ctx.from.username} !*\n`
-    impresion += '👍 *Cotizaciones del dólar*\n'
+    impresion = `*Hola ${ctx.from.username} !*\n\n`
+    impresion += '👍 *Cotizaciones del dólar*\n\n'
     cotizaciones.data.forEach((cotizacion) => {
       const a = moment(ahora)
       const b = moment(cotizacion.fecha_hora)
